58bdf4b25c777d4f07e1a17bf1590c9f52994b3a
Xychic <44938917+Xychic@users.noreply.github.com>
2019-02-08 12:08:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Python libraries that we need to import for our bot
 import os
-from datetime import datetime
+import functions
 from flask import Flask, request
 from pymessenger.bot import Bot
 
@@ -8,47 +8,6 @@ app = Flask(__name__)
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
-
-data = [1,2,3,4,5]
-
-functionDescription = {
-	"LIST-ALL" : "Will list all data and functions",
-	"LIST-FUNC" : "Will list all functions",
-	"LIST-DATA" : "Will list all data",
-        "ECHO" : "Will repeat the message back"
-}
-
-def listAll(inputData):
-    result = "FUNCTIONS"
-    for function in functionDescription:
-        result += "\n" + function + " : " + functionDescription[function]
-    result += "\nDATA"
-    for datum in data:
-        result += "\n" + str(datum)
-    return result
-
-def listFunc(inputData):
-    result = "FUNCTIONS"
-    for function in functionDescription:
-        result += "\n" + function + " : " + functionDescription[function]
-    return result
-
-def listData(inputData):
-    result = "DATA"
-    for datum in data:
-        result += "\n" + str(datum)
-    return result
-
-def echo(inputData):
-    return ("Received '{0}' at {1}").format(" ".join(inputData), str(datetime.now()))
-
-functionAction = {
-	"LIST-ALL" : listAll,
-	"LIST-FUNC" : listFunc,
-	"LIST-DATA" : listData,
-        "ECHO" : echo
-}
-
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
@@ -87,8 +46,8 @@ def verify_fb_token(token_sent):
 
 def get_message(receivedText = "No text"):
     blocks = receivedText.split(" ")
-    if blocks[0].upper() in functionAction:
-        result = functionAction[blocks[0].upper()](blocks[1:])
+    if blocks[0].upper() in functions.functionAction:
+        result = functions.functionAction[blocks[0].upper()](blocks[1:])
     else:
         result = """ERROR:
 Unknown command:'{0}'
